4a6f39e4792352a72378881610462c3d907295bc
Rafael <rafaelsobral284@gmail.com>
2026-05-15 12:36:44 -0300

    comentarios

--- menuJovi.py
+++ menuJovi.py
@@ -1,11 +1,11 @@
-def retornaMenu():
+def retornaMenu(): #Funcao que te faz retornar ao menu principal digitando qualquer número
     sair = int(input("===Digite qualquer número para retornar ao menu===: "))
     if sair >= 0:
          menu()
     if sair < 0:
         print("Digite um número válido")  
 
-def funcionalidades():
+def funcionalidades(): #Funcao que mostra as principais funcionalidades no modo SLID utilizando a camera
         print("=== FUNÇÕES SLID ===")
         print("See, Listen and Identify")
         print("See: Vê, captura o quadro apenas com a camera ativada")
@@ -13,7 +13,7 @@ def funcionalidades():
         print("Identify: Identifica, destasca o essencial da aula")
         retornaMenu()
 
-def entraNoModo():
+def entraNoModo(): #Funcao que simula de forma bem simples como que a modo SLID ira funcionar. Digitando 1 para ativar o modo ou 0 para voltar
     print("Bem-vido(a) ao modo SLID!")
     iniciar = int(input("Digite 1 para iniciar ou 0 para voltar ao menu: "))
     if iniciar == 1:
@@ -28,7 +28,8 @@ def entraNoModo():
 
 materias = ["Front-end ", "Cálculo", "Python", "Web-Development"]
 
-def galeriaOrganizada():
+def galeriaOrganizada(): #funcao que mostra a galeria organizada do modo SLID. Selecionando a materia, ele simula te colocando para a pasta de fotos dessa materia.
+                        #Utizando um for para mostrar a quantidade de materias e as colocando por ordem
     
     for i in range(len(materias)):
         print(f"{i} - {materias[i]}")
@@ -44,7 +45,7 @@ def galeriaOrganizada():
     
     retornaMenu()
         
-def adicionaGaleria():
+def adicionaGaleria(): #Funcao que apos voce digitar uma materia nova no input, ela adiciona essa nova materia na funcao galeriaOrganizada, Fazendo que, voce agora consiga entrar na pasta dessa nova materia
 
     novaMateria = input("Digite o nome da nova matéria: ")
 
@@ -54,7 +55,7 @@ def adicionaGaleria():
 
     retornaMenu()
    
-def desenvolvedores():
+def desenvolvedores():  #Funcao que mostra os nomes dos integrantes do grupo da Sprint como desenvolvedores e nosso email como forma de contato
     print("===Desenvolvedores===")
     print("Erick RIpari Gomes")
     print("Fabricio Denig De Avila")
@@ -71,8 +72,9 @@ def iniciar():
 
 iniciar()
 
-def menu():
-    opcao = 0
+def menu(): # Menu prrincioal feito com while
+            # Escolha a opcao digitando de 1 a 6 no input
+
     while True:
 
         print("=== MENU MODO SLID JOVI ===")
